ec7f1faa0960382065a24f82ac9ee8c45a31c391
Sidvetrivel <sid.vetri@gmail.com>
2023-02-26 19:19:50 -0500

    Str_concat implemented

--- stra.c
+++ stra.c
@@ -4,14 +4,16 @@ size_t Str_getLength(const char pcSrc[])
 {
    size_t uLength = 0;
    assert(pcSrc != NULL);
-   while (pcSrc[uLength] != '\0')
-      uLength++;
+   while (pcSrc[uLength] != '\0'){
+        uLength++;
+   }
    return uLength;
 }
 
-char* Str_copy(char dest[], const char pcSrc[])
+int* Str_copy(char dest[], const char pcSrc[])
 {
     int i = 0;
+    assert(pcSrc != NULL && dest != NULL);
     while(pcSrc[i] != '\0'){
         dest[i] = pcSrc[i];
     }
@@ -19,19 +21,22 @@ char* Str_copy(char dest[], const char pcSrc[])
     return dest;
 }
 
-size_t Str_concat(char dest[], const char pcSrc[])
+int* Str_concat(char dest[], const char pcSrc[])
 {
-
+    int i = 0;
+    int j = 0;
+    assert(pcSrc != NULL && dest != NULL);
+    while(dest[i] != '\0'){
+        i++;
+    }
+    while(pcSrc[j] != '\0'){
+        dest[i] = pcSrc[j];
+        i++;
+        j++;
+    }
+    dest[i] = '\0';
+    return dest;
 }
 
-size_t Str_compare(const char pcSrc[])
-{
-
-}
-
-size_t Str_search(const char pcSrc[])
-{
-
-}
 
 
