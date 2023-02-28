926c0947c1421a207a2b788d6d2ba82ca4223104
Sidvetrivel <sid.vetri@gmail.com>
2023-02-28 15:34:26 -0500

    header file commit

---
+++ str.h
@@ -0,0 +1,19 @@
+#ifndef STRA_INCLUDED
+#define STRA_INCLUDED
+
+//getLength
+size_t Str_getLength(const char pcSrc[]);
+
+//copy
+char* Str_copy(char dest[], const char pcSrc[]);
+
+//concat
+char* Str_concat(char dest[], const char pcSrc[]);
+
+//compare
+int Str_compare(const char str1[], const char pcSrc[]);
+
+//search
+char* Str_search(char str1[], const char pcSrc[]);
+
+#endif
