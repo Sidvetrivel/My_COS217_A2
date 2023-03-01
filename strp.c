3fd21127593399f850f95985d4c442b23f41b7c3
Sidvetrivel <sid.vetri@gmail.com>
2023-02-28 23:13:16 -0500

    new Str_search implementation

--- strp.c
+++ strp.c
@@ -49,8 +49,8 @@ char* Str_search(const char* str1, const char* str2)
     char* i;
     char* j;
     assert(str1 != NULL && str2 != NULL);
-    i = str1;
-    j = str2;
+    i = (char*)str1;
+    j = (char*)str2;
     if(*str2 == '\0'){
         return (char*)str1;
     }
@@ -62,7 +62,7 @@ char* Str_search(const char* str1, const char* str2)
         if(*j == '\0'){
             return (char*)str1;
         }
-        str1++;
+        i++;
     }
     return NULL;
 }
