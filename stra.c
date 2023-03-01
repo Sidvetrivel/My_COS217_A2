232ec617ad9ccf729ed21b9dd318b172f66439e2
Sidvetrivel <sid.vetri@gmail.com>
2023-02-28 21:10:02 -0500

    null checker fix

--- stra.c
+++ stra.c
@@ -56,7 +56,7 @@ char* Str_search(const char str1[], const char pcSrc[])
     int i = 0;
     int j = 0;
     assert(pcSrc != NULL && str1 != NULL);
-    if (str1 == NULL || pcSrc == NULL) {
+    if (str1 == '\0' || pcSrc == '\0') {
         return NULL;
     }
     for (i = 0; str1[i]; i++) {
